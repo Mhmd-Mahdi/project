539832d88ac4f58b75594b3ba1903761492cfbbd
Mhmd-Hmdn <Mhmd.hmdn010@gmail.com>
2025-02-11 01:27:58 +0200

    Update login.php

--- login.php
+++ login.php
@@ -5,7 +5,7 @@
     $db_pass = "";
     $db_name = "users"; 
     $conn = "";
-
+     
     try {
         $conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
     } catch (mysqli_sql_exception $e) {
